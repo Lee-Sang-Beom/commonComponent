80291e2438e88d98d84edfb1718afb50619526e4
SunhyeKim <tjsgp43@gmail.com>
2024-04-24 17:59:05 +0900

    체크박스 라디오 박스 추가

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -2,7 +2,9 @@
 
 import style from "./Form.module.scss";
 import Button from "@/components/Button/Button";
+import Checkbox from "@/components/Checkbox/Checkbox";
 import Input from "@/components/Input/Input";
+import Radiobox from "@/components/Radiobox/Radiobox";
 import Textarea from "@/components/Textarea/Textarea";
 
 export default function FormPage() {
@@ -48,6 +50,12 @@ export default function FormPage() {
             border={"br_round"}
             onClickEvent={() => {}}
           />
+          <Button
+            text={"disabled"}
+            color={"disabled"}
+            id={"btnBasicMd"}
+            onClickEvent={() => {}}
+          />
         </div>
       </div>
 
@@ -84,6 +92,165 @@ export default function FormPage() {
             inpSize={"xsm"}
             border={"br_round"}
           />
+          <Input title={""} placeholder={"disabled"} color={"disabled"} />
+        </div>
+      </div>
+
+      {/* 체크 박스 --------------------------- */}
+      <div className={style.box}>
+        <h3>CHECKBOX</h3>
+        <div className={style.inner}>
+          <Checkbox title={""} inpSize="xlg" border="br_square_round_1" />
+          <Checkbox
+            title={""}
+            inpSize="lg"
+            color={"black"}
+            border="br_square_round_2"
+          />
+          <Checkbox title={""} />
+          <Checkbox
+            title={""}
+            inpSize="sm"
+            color={"mainColor"}
+            border="br_square_round_2"
+          />
+          <Checkbox
+            title={""}
+            inpSize="xsm"
+            color={"mainColor"}
+            border="br_round"
+          />
+          <Checkbox title={""} color={"disabled"} checked={true} />
+        </div>
+      </div>
+
+      {/* 라디오 박스 --------------------------- */}
+      <div className={`${style.box} ${style.radio_box}`}>
+        <h3>RADIOBOX</h3>
+        <div className={style.inner}>
+          <Radiobox
+            title={"가"}
+            inpSize="xlg"
+            items={[
+              {
+                name: "사과",
+                value: "apple",
+                id: "apple",
+              },
+              {
+                name: "바나나",
+                value: "banana",
+                id: "banana",
+              },
+            ]}
+          />
+
+          <Radiobox
+            title={"나"}
+            inpSize="lg"
+            color={"black"}
+            items={[
+              {
+                name: "사과",
+                value: "apple",
+                id: "apple",
+              },
+              {
+                name: "바나나",
+                value: "banana",
+                id: "banana",
+              },
+            ]}
+          />
+
+          <Radiobox
+            title={"다"}
+            items={[
+              {
+                name: "사과",
+                value: "apple",
+                id: "apple",
+              },
+              {
+                name: "바나나",
+                value: "banana",
+                id: "banana",
+              },
+            ]}
+          />
+
+          <Radiobox
+            title={"라"}
+            inpSize="sm"
+            color={"mainColor"}
+            items={[
+              {
+                name: "사과",
+                value: "apple",
+                id: "apple",
+              },
+              {
+                name: "바나나",
+                value: "banana",
+                id: "banana",
+              },
+            ]}
+          />
+
+          <Radiobox
+            title={"마"}
+            inpSize="xsm"
+            color={"mainColor"}
+            items={[
+              {
+                name: "사과",
+                value: "apple",
+                id: "apple",
+              },
+              {
+                name: "바나나",
+                value: "banana",
+                id: "banana",
+              },
+            ]}
+          />
+
+          <Radiobox
+            title={"바"}
+            color={"black"}
+            items={[
+              {
+                name: "사과",
+                value: "apple",
+                id: "apple",
+                disabled: true,
+              },
+              {
+                name: "바나나",
+                value: "banana",
+                id: "banana",
+                checked: true,
+              },
+            ]}
+          />
+
+          <Radiobox
+            title={"사"}
+            color={"disabled"}
+            items={[
+              {
+                name: "사과",
+                value: "apple",
+                id: "apple",
+                checked: true,
+              },
+              {
+                name: "바나나",
+                value: "banana",
+                id: "banana",
+              },
+            ]}
+          />
         </div>
       </div>
 
@@ -119,6 +286,7 @@ export default function FormPage() {
             taSize={"xsm"}
             border={"br_round"}
           />
+          <Textarea title={""} placeholder={"disabled"} color={"disabled"} />
         </div>
       </div>
 
