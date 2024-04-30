def1aec8cd7269ef99eb2f6c8baea103f964498b
sys8517 <sys8517@naver.com>
2024-04-30 13:11:39 +0900

    테이블 요소 작업중

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -1,3 +1,29 @@
+import Table, { TableHeader } from "@/components/Table/Table";
+
 export default function TablePage() {
-    return <></>;
+    const header: TableHeader[] = [
+        {
+            name: "num",
+            value: "rowId",
+        },
+        {
+            name: "col1",
+            value: "col1",
+        },
+        {
+            name: "col2",
+            value: "col2",
+        },
+    ];
+    const data = [
+        { rowId: "1", col1: "값1", col2: "값2" },
+        { rowId: "2", col1: "값2", col2: "값2" },
+        { rowId: "3", col1: "값3", col2: "값3" },
+    ];
+    return (
+        <>
+            {/* 서치박스 넣을 자리 */}
+            <Table data={data} headers={header} tableType={"vertical"} tableCaption={""} itemTitle={""} ref={null} />
+        </>
+    );
 }
