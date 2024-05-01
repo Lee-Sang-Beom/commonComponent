22d71f10f25bc2c635d2e4e243f174448f27feb6
sys8517 <sys8517@naver.com>
2024-05-02 08:55:17 +0900

    메인 수정

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,18 @@
 import style from "./page.module.scss";
 
 export default function Home() {
-  return <main className={style.main}></main>;
+    return (
+        <main className={style.main}>
+            {" "}
+            <p
+                style={{
+                    textAlign: "center",
+                    fontSize: "250px",
+                    marginTop: "200px",
+                }}
+            >
+                ദ്ദിㆆ_ㆆ&#41;
+            </p>
+        </main>
+    );
 }
