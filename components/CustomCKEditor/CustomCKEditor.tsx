a4d6d7e7257469b2f7cafcf8584a9d3a603791d4
이상범 <tkdqja975@naver.com>
2024-05-16 10:19:08 +0900

    rebuilding ckeditor5

--- components/CustomCKEditor/CustomCKEditor.tsx
+++ components/CustomCKEditor/CustomCKEditor.tsx
@@ -6,8 +6,8 @@ import React, { useRef } from "react";
 import * as process from "process";
 import FormData from "form-data";
 import axios from "axios";
-// import Editor from "ckeditor5-custom-build/build/ckeditor";
-// import { CKEditor } from "@ckeditor/ckeditor5-react";
+import Editor from "ckeditor5-custom-build/build/ckeditor";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
 
 interface CustomCKEditorProps {
   /**
@@ -21,57 +21,57 @@ interface CustomCKEditorProps {
 }
 
 const CustomCKEditor = ({ content, onContentChange }: CustomCKEditorProps) => {
-  // const customUploadAdapter = (loader: any) => {
-  //   return {
-  //     upload: () => {
-  //       return new Promise((resolve, reject) => {
-  //         const data = new FormData();
-  //         loader.file.then((file: any) => {
-  //           data.append("file", file);
-  //           axios
-  //             .post(
-  //               process.env.NEXT_PUBLIC_HOST + "/utils/files/upload",
-  //               data,
-  //               {
-  //                 headers: {
-  //                   "Content-Type":
-  //                     "multipart/form-data; boundary=" +
-  //                     "------" +
-  //                     Math.random().toString(36).substring(2),
-  //                 },
-  //               }
-  //             )
-  //             .then((res) => {
-  //               const { data } = res;
-  //               if (data.error) {
-  //                 reject(data.error);
-  //               } else {
-  //                 resolve({
-  //                   default: data.url,
-  //                 });
-  //               }
-  //             })
-  //             .catch((err) => {
-  //               reject(err);
-  //             });
-  //         });
-  //       });
-  //     },
-  //     abort: () => {},
-  //   };
-  // };
+  const customUploadAdapter = (loader: any) => {
+    return {
+      upload: () => {
+        return new Promise((resolve, reject) => {
+          const data = new FormData();
+          loader.file.then((file: any) => {
+            data.append("file", file);
+            axios
+              .post(
+                process.env.NEXT_PUBLIC_HOST + "/utils/files/upload",
+                data,
+                {
+                  headers: {
+                    "Content-Type":
+                      "multipart/form-data; boundary=" +
+                      "------" +
+                      Math.random().toString(36).substring(2),
+                  },
+                }
+              )
+              .then((res) => {
+                const { data } = res;
+                if (data.error) {
+                  reject(data.error);
+                } else {
+                  resolve({
+                    default: data.url,
+                  });
+                }
+              })
+              .catch((err) => {
+                reject(err);
+              });
+          });
+        });
+      },
+      abort: () => {},
+    };
+  };
 
-  // function uploadPlugin(editor: any) {
-  //   editor.plugins.get("FileRepository").createUploadAdapter = (
-  //     loader: any
-  //   ) => {
-  //     return customUploadAdapter(loader);
-  //   };
-  // }
+  function uploadPlugin(editor: any) {
+    editor.plugins.get("FileRepository").createUploadAdapter = (
+      loader: any
+    ) => {
+      return customUploadAdapter(loader);
+    };
+  }
 
   return (
     <>
-      {/* <CKEditor
+      <CKEditor
         editor={Editor}
         data={content}
         onChange={(e, editor) => {
@@ -91,7 +91,7 @@ const CustomCKEditor = ({ content, onContentChange }: CustomCKEditorProps) => {
             previewsInData: true,
           },
         }}
-      /> */}
+      />
     </>
   );
 };
